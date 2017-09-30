027145f9a85d501ecdb2774c5f95b93505fd9c82
sumitkau <sumit.kau@gmail.com>
2017-09-30 22:54:09 +0530

    contrail

--- kubeadm-contrail/roles/2-master-init/files/pods-waiting-check0.sh
+++ kubeadm-contrail/roles/2-master-init/files/pods-waiting-check0.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-waiting_pod=`kubectl get pods -n kube-system |awk '{print $3}'|egrep  -v "^Running|^STATUS"| wc -l`
+waiting_pod=`kubectl get pods -n kube-system |awk '{print $3}'| wc -l`
 #waiting_pod=2
 
 while [ "$waiting_pod" > 0 ];
 do
-    waiting_pod=`kubectl get pods -n kube-system |awk '{print $3}'|egrep  -v "^Running|^STATUS"| wc -l`
+    waiting_pod=`kubectl get pods -n kube-system |awk '{print $3}'|grep -i Running| wc -l`
 #    waiting_pod=2
-    if [ "$waiting_pod" == 0 ]
+    if [ "$waiting_pod" == 5 ]
     then
        echo "Pods got initialized" > /tmp/pod-status-check.txt
        rm -rf /tmp/pod-status-check.txt;
